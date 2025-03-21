b21ee824354efdcbe9759b024d8bfb3f50b201d9
PysJlaH_JlT <ruslan160306@gmail.com>
2025-03-21 16:35:31 +0200

    Добавлено функцію руху заднього фону при руху курсору, а також анімацію зірок над надписом 'UniversalRestaurant' при заході на головну сторінку

--- universalrestaurant/restaurant/static/restaurant/js/main_page.js
+++ universalrestaurant/restaurant/static/restaurant/js/main_page.js
@@ -3,13 +3,20 @@ document.addEventListener("DOMContentLoaded", function () {
     const dropdownMenu = document.getElementById("dropdown-menu");
 
     menuButton.addEventListener("click", function () {
-        dropdownMenu.classList.toggle("hidden"); // Перемикає клас, що приховує меню
+        dropdownMenu.classList.toggle("hidden");
     });
-
 
     document.addEventListener("click", function (event) {
         if (!menuButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
             dropdownMenu.classList.add("hidden");
         }
+    });
+
+
+    document.addEventListener("mousemove", (event) => {
+        let moveX = (event.clientX / window.innerWidth - 0.5) * 10;
+        let moveY = (event.clientY / window.innerHeight - 0.5) * 10;
+
+        document.body.style.backgroundPosition = `${50 + moveX}% ${50 + moveY}%`;
     });
 });
