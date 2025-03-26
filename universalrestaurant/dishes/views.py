a97196da67d30b150a18304f576782047a6ddfb1
PysJlaH_JlT <ruslan160306@gmail.com>
2025-03-26 15:39:08 +0200

    Додано базову структуру та меню до каталогу страв 'Звичайні страви'

--- universalrestaurant/dishes/views.py
+++ universalrestaurant/dishes/views.py
@@ -10,16 +10,20 @@ nav_buttons = [{'title': 'Головна сторінка', 'ref':'main_page'},
                {'title': 'Зареєструватися', 'ref':'login'},
                {'title':'Каталог Страв', 'food_buttons':food_buttons}]
 
-def render_food_page(request, title, template_name):
+from dishes.utils import json_format
+
+
+def render_food_page(request, title, template_name, **kwargs):
     data = {
         'title': title,
         'nav_buttons': nav_buttons,
-        'food_buttons': food_buttons
+        'food_buttons': food_buttons,
+        **kwargs
     }
     return render(request, template_name, context=data)
 
 def normal_food(request):
-    return render_food_page(request, 'Звичайні страви', 'normal_food/index.html')
+    return render_food_page(request, 'Звичайні страви', 'normal_food/index.html', food=json_format.n_dishes)
 
 def sea_food(request):
     return render_food_page(request, 'Морські страви', 'sea_food/index.html')
